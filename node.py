c756cd966bcf8e4fcc64564cfbf29ecd60851b5a
Fuheng Zhao <fuheng_zhao@umail.ucsb.edu>
2021-11-11 22:28:48 -0800

    Add Node Class

---
+++ node.py
@@ -0,0 +1,7 @@
+class Complex:
+	def __init__(self, server, id, linkage = []):
+		self.server = server
+		self.id = id
+		self.linkage = linkage
+	def connect(self, nxt_server, nxt_id):
+		self.linkage.append((nxt_server, nxt_id))

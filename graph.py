a0294268e2cc82070df05a11ae58bfcf0df86904
Charlie110 <fuheng_zhao@umail.ucsb.edu>
2021-11-17 14:50:33 -0800

    add dfs method

--- graph.py
+++ graph.py
@@ -21,3 +21,83 @@ class Graph:
                         print("connect local nodes fail: invalid ID")
                         continue
                     self.nodes[message.message[0]].connect(self.pid, message.message[1])
+    def hasReference(self, nid):
+        visited = {}
+        arr = [self.nodes(nid)]
+        visited.add(nid)
+        while len(arr)!=0:
+            node = arr.pop()
+            if node.isConnected():
+                return True
+            for (pid, nid) in node.neighbours:
+                if pid != self.pid or nid in visited:
+                    continue
+                visited.add(nid)
+                arr.append(self.nodes(nid))                
+        return False
+
+    def outsideConnections(self, nid):
+        connections = []
+        visited = {}
+        arr = [self.nodes(nid)]
+        visited.add(nid)
+        while len(arr)!=0:
+            node = arr.pop()
+            for (pid, nid) in node.neighbours:
+                if nid in visited:
+                    continue
+                if pid != self.pid:
+                    connections.append((pid, nid))
+                else:
+                    visited.add(nid)
+                    arr.append(self.nodes(nid))                
+        return connections 
+
+    def cycle_detection(self, sids, rids, visited):
+        assert(rids[0] == self.pid)
+        receiver = self.nodes[rids[0]]
+        if self.hasReference(receiver):
+            # send False to sender, as a local reference is found
+            send(sids, False)
+            # abort
+            return
+        if receiver in visited:
+            # send True to sender, as a cycle is found
+            send(sids, True)
+        visited.add(rids)
+        
+        for (pid, nid) in receiver.outsideConnections():
+            send((pid, nid), "cycle detection", visited)
+            if recv((pid, nid)) == False:
+                send(sids, False)
+                # abort
+                return
+        # after waiting for all senders
+        send(sids, True)
+        
+        # this is the cycle detection starter
+        if not sids:
+            self.remove(rids)
+        return
+    
+    def remove(self, sids, rids, visited):
+        # inform other sever to do GC
+        if rids not in visited:
+            visited.add(rids)
+            for (pid, nid) in self.nodes[rids[1]].outsideConnections():
+                send((pid, nid), "remove", visited)
+                visited = {}
+        # perform local GC
+        arr = [rids[0]]
+        while len(arr)!=0:
+            id = arr.pop()
+            if id not in self.nodes:
+                continue
+            for (pid, nid) in self.nodes[nid].neighbours:
+                if pid == self.pid:
+                    arr.append(nid) 
+                del self.nodes[id]               
+        return
+            
+        
+
--- node.py
+++ node.py
@@ -1,8 +1,18 @@
 class Node:
-    def __init__(self, server, id, linkage = []):
+    def __init__(self, server, id, connected=True, outVertices = []):
     	self.server = server
         self.id = id
-        self.linkage = linkage
+        self.root = connected
+        self.outVertices = outVertices
     
+    def removeNode(self):
+        self.root = False
+
     def connect(self, nxt_server, nxt_id):
-        self.linkage.append((nxt_server, nxt_id))
+        self.outVertices.append((nxt_server, nxt_id))
+
+    def neighbours(self):
+        return self.outVertices
+    
+    def isConnected(self):
+        return self.root
